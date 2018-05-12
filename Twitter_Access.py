8a59dbbacb74541b899c7a10787b5aa2b7b0e902
Alison Kline <akline@iee.org>
2018-05-11 22:44:12 -0700

    Writing data to csv
    first attempt at handling rate limits

--- Twitter_Access.py
+++ Twitter_Access.py
@@ -1,6 +1,8 @@
 # exploring twitter's API reference https://chatbotslife.com/twitter-data-mining-a-guide-to-big-data-analytics-using-python-4efc8ccfa219
 import tweepy       #http://docs.tweepy.org/en/v3.5.0/api.html
 import csv
+import time
+from datetime import datetime
 
 def get_access_tokens():
     """Loading App secrets and keys."""
@@ -43,11 +45,59 @@ def users_timeline(api, id, tweets_to_pull=5):
     print_tweets(results)
 
 def search_tweets(api, query, language="en"):
-    # Calling the user_timeline function with our parameters
+    # Calling the search function with our parameters
     results = api.search(q=query, lang=language)
     print_tweets(results)
+
+def getcsvwriter(filename, fields):
+    w = csv.DictWriter(open(filename, "w"), fields)
+    w.writeheader()
+    return w
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            print('RateLimetError at : {}, waiting 15 mins...'.format(datetime.now()))
+            time.sleep(15 * 60)  #wait 15mins for the limit to refresh
+
+def get_followers(api, id):
+    fields=['screen_name','followers_count', 'following', 'created_at', 'location', 'default_profile', 'default_profile_image',\
+            'description', 'favourites_count', 'friends_count', 'geo_enabled', 'has_extended_profile', 'lang', 'name',\
+            'profile_use_backgroud_image', 'statuses_count']
+    out = getcsvwriter('data/followersOf'+id+'.csv', fields)
+    rows= {}
+    # Calling the paginated followers function with provided parameters
+    for followers in limit_handled(tweepy.Cursor(api.followers, id=id).pages()):
+        # Calling the followers function with provided parameters
+        for fol in followers:
+            if fol.lang != 'en':            # skipping followers who don't have english as their set language
+                continue
+            row = {
+                fields[0]: fol.screen_name,
+                fields[1]: fol.followers_count,
+                fields[2]: fol.following,
+                fields[3]: str(fol.created_at),
+                fields[4]: fol.location,
+                fields[5]: fol.default_profile,
+                fields[6]: fol.default_profile_image,
+                fields[7]: fol.description,
+                fields[8]: fol.favourites_count,
+                fields[9]: fol.friends_count,
+                fields[10]: fol.geo_enabled,
+                fields[11]: fol.has_extended_profile,
+                fields[12]: fol.lang,
+                fields[13]: fol.name,
+                fields[14]: fol.profile_use_background_image,
+                fields[15]: fol.statuses_count,
+            }
+            out.writerow(row)
+            print('SName: {}, has {} followers and {} friends'.format(fol.screen_name, fol.friends_count, fol.following))
+            print('    Created in: {}, from {}'.format(str(fol.created_at), fol.location))
 
 api = get_twitter_api_obj(get_access_tokens())
 # my_home_timeline(api)
 # users_timeline(api, "nytimes", 2)
-search_tweets(api, "Toptal")
+# search_tweets(api, "Toptal")
+get_followers(api, 'GOP')
