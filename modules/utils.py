7f18e8fedc36752deef42ea19476f038e25acf08
Alison Kline <akline@iee.org>
2018-05-22 17:44:39 -0700

    more PEP 8 warnings

--- modules/utils.py
+++ modules/utils.py
@@ -4,6 +4,7 @@ csvwriter for provided filename and fields.
 """
 import csv
 
+
 def get_csv_writer(filename, fields):
     """Returns a csv DictWriter with provided filename and header already write with provided fields."""
     w = csv.DictWriter(open(filename, "w"), fields)
